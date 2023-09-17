d801daecf19dd968b77aafc9d16f87e1ec2d4273
Alexander <92460691+san27sin@users.noreply.github.com>
2023-09-17 22:40:02 +0300

    lesson2

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,12 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <primary-button title="создать"></primary-button>
+    <router-view />
 </template>
 
+<script setup lang="ts">
+import PrimaryButton from '../src/shared/ui/PrimaryButton/PrimaryButton';
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import '../src/shared/css/index.css';
 </style>
---
+++ src/shared/ui/PrimaryButton/PrimaryButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <button>
+        {{ title }}
+    </button>
+</template>
+
+<script setup lang="ts">
+interface IProps {
+    title: String;
+}
+const props = defineProps<IProps>();
+</script>
+
+<style lang="scss" scoped>
+.button {
+    background: #030319;
+    padding: 19px 38px;
+}
+</style>
