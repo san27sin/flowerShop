5ed0ac22407808f21ae41cf83963cb2540b70704
Alexander <coder27sinitsyn@gmail.com>
2024-01-30 21:33:19 +0300

    authorization
    Остановился на создание страницы авторизации.

---
+++ src/pages/AuthView.vue
@@ -0,0 +1,19 @@
+<template>
+  <form class="form" action="">
+    <div class="form__container">
+      <img src="@/assets/Logo.svg" alt="">
+      <UiInput placeholder="email"></UiInput>
+      <UiInput placeholder="nickname"></UiInput>
+      <UiInput placeholder="password"></UiInput>
+      <PrimaryButton ttile="Зарегистрироваться"></PrimaryButton>
+    </div>
+  </form>
+</template>
+
+<style scoped lang="scss">
+
+</style>
+
+<script setup lang="ts">
+// TODO сделать самим верстку страницу входа
+</script>
